36e7f4e89147a38b4061e7b89ac44fc018f8afc4
albert <albert3787@gmail.com>
2022-04-19 15:20:26 +0800

    add marked gem

--- app/javascript/controllers/markdown-editor_controller.js
+++ app/javascript/controllers/markdown-editor_controller.js
@@ -1,16 +1,16 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "stimulus"
 import EasyMDE from "easymde"
+import "easymde/dist/easymde.min.css"
+
 export default class extends Controller {
+  initialize() {
+    this.editor = null
+  }
   connect() {
-    new EasyMDE({ element: this.element })
+    this.editor = new EasyMDE({ element: this.element, spellChecker: false })
+  }
+  disconnect() {
+    this.editor.toTextArea()
+    this.editor = null
   }
 }
